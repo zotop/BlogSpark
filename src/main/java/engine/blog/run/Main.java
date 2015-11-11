cffbfcf93fa22a116e5d2e0e8cb975856001fc24
Joao Guerreiro <jmdguerreiro@gmail.com>
2015-11-12 00:03:15 +0100

    serving static files example

--- src/main/java/engine/blog/run/Main.java
+++ src/main/java/engine/blog/run/Main.java
@@ -4,11 +4,14 @@ package engine.blog.run;
 import spark.servlet.SparkApplication;
 
 import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class Main implements SparkApplication {
 
     @Override
     public void init() {
+        staticFileLocation("public");
         get("/hello", (req, res) -> "Hello World");
+        //get("/", (req, res) -> "hi");
     }
 }
