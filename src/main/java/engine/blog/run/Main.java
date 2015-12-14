f8b6445a51a0dc42d075ea845306244a2337fa6c
Joao Guerreiro <jmdguerreiro@gmail.com>
2015-12-14 11:30:40 +0100

    mock login page that calls the backend api

--- src/main/java/engine/blog/run/Main.java
+++ src/main/java/engine/blog/run/Main.java
@@ -4,6 +4,7 @@ package engine.blog.run;
 import com.mongodb.DB;
 import engine.blog.db.MongoDBClient;
 import engine.blog.route.PostRoute;
+import engine.blog.route.UserRoute;
 import spark.servlet.SparkApplication;
 
 import static spark.Spark.staticFileLocation;
@@ -15,5 +16,6 @@ public class Main implements SparkApplication {
         staticFileLocation("public");
         DB db = MongoDBClient.INSTANCE.getDatabase();
         new PostRoute(db);
+        new UserRoute(db);
     }
 }
